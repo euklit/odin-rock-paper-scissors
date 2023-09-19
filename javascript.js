d1406beec3a86d9256ecb7c722ba72f6eb978850
euklit <n.lieven@outlook.com>
2023-09-19 20:13:39 +0200

    Add basic UI and rework old game logic to work w/ UI

--- javascript.js
+++ javascript.js
@@ -1,16 +1,50 @@
-winner = ["tie", "player", "computer"]
+let winner = ["tie", "player", "computer"]
+let scoreArr = [0,0]
+
+
+const choicePara = document.querySelector('#choice')
+const scorePara = document.querySelector('#score')
+const winnerPara = document.querySelector('#winner')
+
+const controls = document.querySelectorAll('button');
+controls.forEach(control => control.addEventListener('click',playRound))
+
 
 function getComputerChoice() {
     possibleChoices = ["Rock", "Paper", "Scissors"];
     return possibleChoices[Math.floor(Math.random()*3)];
 }
 
-console.log(getComputerChoice());
 
-function playRound(computerChoice, playerChoice) {
-    computerChoice = computerChoice.toLowerCase();
-    playerChoice = playerChoice.toLowerCase();
+function playRound(btn) {
+    if(scoreArr[0] == 5 || scoreArr[1] == 5) {
+        winnerPara.textContent = "";
+        scoreArr = [0,0]
+    }
 
+    let computerChoice = getComputerChoice().toLowerCase();
+    playerChoice = this.getAttribute("id");
+
+    roundWinner = findWinner(computerChoice,playerChoice);
+    calcScore(roundWinner);
+
+    choicePara.textContent = `You picked ${playerChoice}, the computer picked ${computerChoice}.\n `
+    scorePara.textContent = `You ${scoreArr[0]} : ${scoreArr[1]} Computer`
+    
+    if(scoreArr[0] == 5 || scoreArr[1] == 5) {
+        if (roundWinner == winner[0]) {
+            winnerPara.textContent = "Tie!"
+        } else if (roundWinner == winner[1]) {
+            winnerPara.textContent = "You won! :)"
+        } else {
+            winnerPara.textContent = "You lost :("
+        }
+
+    }
+}
+
+
+function findWinner(computerChoice, playerChoice) {
     if (computerChoice == playerChoice) {
         return winner[0]
     } else if ((playerChoice == "rock" && computerChoice == "scissors") ||
@@ -22,42 +56,18 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
-function game() { // play five rounds 
-    let scoreArr = [0,0] // first slot: player; second slot: computer
 
-
-    for(i=0;i<5;i++) {
-        const playerChoice = window.prompt("Pick rock, paper or scissors","");
-        const roundWinner = playRound(getComputerChoice(),playerChoice);
-        scoreArr = calcScore(roundWinner,scoreArr);
-        console.log(`Player score: ${scoreArr[0]} - computer score ${scoreArr[1]}`);
-    }
-
-    if (scoreArr[0] > scoreArr[1]) {
-        console.log("You won!")
-    } else if (scoreArr[1] > scoreArr[0]) {
-        console.log("You lost!")
-    } else {
-        console.log("Tie!")
-    }
-
-}
-
-function calcScore(roundWinner, scoreArr) {
+function calcScore(roundWinner) {
     switch (roundWinner) {
-        case winner[0]:
-            scoreArr[0]++;
-            scoreArr[1]++;
-            break;
         case winner[1]:
-            scoreArr[0];
+            scoreArr[0]++;
             break;
         case winner[2]:
             scoreArr[1]++;
             break;
     }
-    return scoreArr;
+    
 }
 
-game();
+
 
